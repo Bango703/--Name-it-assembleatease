449eab53deff50b3f610a82be56b34672a2e4ab1
Developer <developer@example.com>
2026-04-09 12:45:19 -0500

    Add Supabase anon key

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ export default {
   apiBase: '',
   supabase: {
     url: 'https://ukamyqnaukxlmeoncjgr.supabase.co',
-    anonKey: '' // Add your Supabase anon key here
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrYW15cW5hdWt4bG1lb25jamdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NDUzMDMsImV4cCI6MjA5MTMyMTMwM30.WMLkxSGm5mnvGfx7gcvf7UKfefjxQf8yOZvk_HzZQNY'
   }
 };
