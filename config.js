a8c6e6b1893684d22288e8b5f4d6aea4af7c0a82
Developer <developer@example.com>
2026-04-09 13:46:13 -0500

    Update config.js with Supabase client

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
-// Project configuration
-export default {
-  siteName: 'Handyman Marketplace',
-  apiBase: '',
-  supabase: {
-    url: 'https://ukamyqnaukxlmeoncjgr.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrYW15cW5hdWt4bG1lb25jamdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NDUzMDMsImV4cCI6MjA5MTMyMTMwM30.WMLkxSGm5mnvGfx7gcvf7UKfefjxQf8yOZvk_HzZQNY'
-  }
-};
+const supabase = window.supabase.createClient(
+  'https://ukamyqnaukxlmeoncjgr.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrYW15cW5hdWt4bG1lb25jamdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NDUzMDMsImV4cCI6MjA5MTMyMTMwM30.WMLkxSGm5mnvGfx7gcvf7UKfefjxQf8yOZvk_HzZQNY'
+)
